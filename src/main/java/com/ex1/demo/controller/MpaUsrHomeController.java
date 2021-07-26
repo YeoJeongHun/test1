ba1f1a5219afc03807b7f8c3cc4e2a45469b9601
unknown <aofhd0003@naver.com>
2021-07-26 22:11:35 +0900

    이미지 업로드 성공

--- src/main/java/com/ex1/demo/controller/MpaUsrHomeController.java
+++ src/main/java/com/ex1/demo/controller/MpaUsrHomeController.java
@@ -1,7 +1,17 @@
 package com.ex1.demo.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
+
+import com.ex1.demo.dto.ResultData;
 
 @Controller
 public class MpaUsrHomeController {
@@ -14,4 +24,38 @@ public class MpaUsrHomeController {
 	public String showMain() {
 		return "mpaUsr/home/main";
 	}
+
+	// 아래부터 테스트용
+	@RequestMapping("/mpaUsr/home/uploadFile")
+	public String uploadFile(HttpServletRequest req, MultipartRequest multipartRequest) {
+		Map<String, MultipartFile> fileMap = multipartRequest.getFileMap();
+				
+		for (String fileInputName : fileMap.keySet()) {
+			MultipartFile multipartFile = fileMap.get(fileInputName);
+			req.setAttribute("test", multipartFile.getOriginalFilename());
+			
+			// 새 파일이 저장될 폴더(io파일) 객체 생성
+	        String targetDirPath = "C:/work/insta-usr-sb-file";
+	        java.io.File targetDir = new java.io.File(targetDirPath);
+
+	        // 새 파일이 저장될 폴더가 존재하지 않는다면 생성
+	        if (targetDir.exists() == false) {
+	            targetDir.mkdirs();
+	        }
+	        
+	        String targetFileName = multipartFile.getOriginalFilename() + "." + "jpg";
+	        String targetFilePath = targetDirPath + "/" + targetFileName;
+
+	        // 파일 생성(업로드된 파일을 지정된 경로롤 옮김)
+	        try {
+	            multipartFile.transferTo(new File(targetFilePath));
+	        } catch (IllegalStateException | IOException e) {
+	            return "mpaUsr/article/detail";
+	        }
+		}
+
+		
+		return "mpaUsr/home/main";
+	}
+
 }
--- src/main/webapp/WEB-INF/jsp/mpaUsr/home/main.jsp
+++ src/main/webapp/WEB-INF/jsp/mpaUsr/home/main.jsp
@@ -7,10 +7,25 @@
 
 <%@ include file="../common/head.jspf"%>
 
-<form method="POST" enctype="multipart/form-data" action="doJoin" onsubmit="MemberJoin__submitForm(this); return false;">
+<form method="POST" enctype="multipart/form-data" action="uploadFile" onsubmit="MemberJoin__submitForm(this); return false;">
 	<div class="form-control">
 		<label class="label"> 프로필 이미지 </label>
 		<input type="file" name="input__file" placeholder="프로필 이미지를 선택해주세요." />
+	</div>
+	<br/>
+	<button type="submit" class="btn btn-primary btn-sm mb-1">
+		<span><i class="fas fa-user-plus"></i></span>
+		&nbsp;
+		<span>전송!</span>
+	</button>
+	
+	<br/>
+	<br/>
+	<br/>
+	결과 : ${test}
+	
+	<div class="flex-shrink-0">
+		<img class="w-10 h-10 object-cover rounded-full shadow mr-2 cursor-pointer" alt="User avatar" src="C:\Users\aofhd\OneDrive\바탕 화면\test.jpg">
 	</div>
 </form>
 
